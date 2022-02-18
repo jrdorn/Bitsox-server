c8480ed90aac1bb7098e5787f2453bae597aa121
Josh Dorn <jr_dorn@yahoo.com>
2022-02-17 20:39:20 -0600

    List Users in Postgres

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,10 @@
 //PostgreSQL db and Sequelize
 
+/**
+ * Id, Email, Password
+ * profile pic, full name, shipping address, 2FA (boolean), newsletter(boolean). wishlist
+ */
+
 // //init Heroku Postgres client
 // const { Client } = require("pg");
 // const client = new Client({
--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 //init node express app
 const path = require("path");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 //access environment variables on localhost
 // require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors()); //CORS
+app.use(bodyParser.json()); // handle application/json requests
+app.use(bodyParser.urlencoded({ extended: true })); // handle application/x-www-form-urlencoded requests
 
 let PORT = process.env.PORT;
 if (PORT === null || PORT === undefined || PORT === "") {
   PORT = 8000;
 }
+
+//
+//
+//
+const { Pool } = require("pg");
+
+//pool object hooked into Postgres db
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/users", (req, res) => {
+  //read from the db: display all data in Users table
+  pool.query(`SELECT * FROM Users;`, (err, res) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(res.rows);
+    }
+  });
+});
+//
+//
+//
 
 //test call
 app.get("/", (req, res) => {
